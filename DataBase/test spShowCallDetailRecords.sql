442701ca1dcdf37650e2ed00535d852764d7375c
Georgy Georgiev <accforgames@abv.bg>
2019-10-22 18:15:02 +0300

    .

---
+++ DataBase/test spShowCallDetailRecords.sql
@@ -0,0 +1,12 @@
+
+
+EXECUTE spShowCallDetailRecords  
+ 1,                     -- @IncludeMale		
+ 1,                     -- @IncludeFemale		
+ 1,                     -- @IncludeUnknowGender
+ 1,                     -- @Include_18_to_25	
+ 1,                     -- @Include_26_to_35	
+ 1,                     -- @Include_36_to_45	
+ 1,                     -- @Include_46_to_65	
+ 1,                     -- @Include_66_to_100	
+ '2016-04-18 03:12:23'  -- @StartDate   
